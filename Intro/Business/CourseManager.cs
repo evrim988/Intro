85aa8b601c3d41a242c9019dfcf4b67ee42e6a90
evrim çalışkan <evrim98@outlook.com>
2024-02-13 17:29:06 +0300

    Add project files.

---
+++ Intro/Business/CourseManager.cs
@@ -0,0 +1,42 @@
+﻿using Intro.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro.Business;
+
+public class CourseManager
+{
+    Course[] courses = new Course[3];
+    
+    Course Course = new Course()
+    {
+        Id = 1,
+        Name = "Test",
+        Description = "Deneme",
+        Price = 10,
+    };
+
+    Course course1 = new Course()
+    {
+        Id = 2,
+        Name = "C#",
+        Description = "C# 2024",
+        Price = 20
+    };
+
+    Course course2 = new Course()
+    {
+        Id = 3,
+        Name = "C++",
+        Description = "C++ 2024",
+        Price = 30
+    };
+
+    public Course[] GetAll()
+    {
+        return courses;
+    }
+}
